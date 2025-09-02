adc1e9484b339477bad21ee8e4b4e621e3bd3197
John Leskas <john.leskas@gmail.com>
2025-09-03 00:12:01 +0300

    single content UI updates

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,23 +1,33 @@
-<article @php(post_class('h-entry'))>
-  <header>
-    <h1 class="p-name">
+<article @php(post_class('h-entry max-w-4xl mx-auto px-4 sm:px-6 lg:px-8'))>
+  <header class="text-center mb-8">
+    <div class="mb-4">
+      @include('partials.entry-meta')
+    </div>
+    
+    <h1 class="p-name text-3xl sm:text-4xl lg:text-5xl font-bold mb-6">
       {!! $title !!}
     </h1>
-
-    @include('partials.entry-meta')
   </header>
 
-  <div class="e-content">
+  @if (has_post_thumbnail())
+    <div class="mb-8 rounded-lg overflow-hidden">
+      @php(the_post_thumbnail('full', ['class' => 'w-full h-auto']))
+    </div>
+  @endif
+
+  <div class="e-content prose prose-lg max-w-none">
     @php(the_content())
   </div>
 
   @if ($pagination)
-    <footer>
+    <footer class="mt-8">
       <nav class="page-nav" aria-label="Page">
         {!! $pagination !!}
       </nav>
     </footer>
   @endif
 
-  @php(comments_template())
+  <div class="mt-12">
+    @php(comments_template())
+  </div>
 </article>
