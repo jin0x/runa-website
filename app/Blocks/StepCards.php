6acf7e7b435f1fae6916aab7c22fba3dfe2103ed
Sergio Cubas <sercub09@gmail.com>
2025-09-19 11:26:14 -0600

    feat: image step cards

---
+++ app/Blocks/StepCards.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace App\Blocks;
+
+use App\Fields\Partials\SectionHeading;
+use App\Fields\Partials\SectionOptions;
+use Log1x\AcfComposer\Block;
+use Log1x\AcfComposer\Builder;
+use Log1x\AcfComposer\Partial; 
+
+class StepCards extends Block
+{
+    /**
+     * The block name.
+     *
+     * @var string
+     */
+    public $name = 'Step Cards';
+
+    /**
+     * The block description.
+     *
+     * @var string
+     */
+    public $description = 'A grid of cards with images, titles, and descriptions for showcasing features, steps or key content.';
+
+    /**
+     * The block category.
+     *
+     * @var string
+     */
+    public $category = 'text';
+
+    /**
+     * The block icon.
+     *
+     * @var string|array
+     */
+    public $icon = 'editor-ul';
+
+    /**
+     * The block keywords.
+     *
+     * @var array
+     */
+    public $keywords = [];
+
+    /**
+     * The block post type allow list.
+     *
+     * @var array
+     */
+    public $post_types = [];
+
+    /**
+     * The parent block type allow list.
+     *
+     * @var array
+     */
+    public $parent = [];
+
+    /**
+     * The ancestor block type allow list.
+     *
+     * @var array
+     */
+    public $ancestor = [];
+
+    /**
+     * The default block mode.
+     *
+     * @var string
+     */
+    public $mode = 'preview';
+
+    /**
+     * The default block alignment.
+     *
+     * @var string
+     */
+    public $align = '';
+
+    /**
+     * The default block text alignment.
+     *
+     * @var string
+     */
+    public $align_text = '';
+
+    /**
+     * The default block content alignment.
+     *
+     * @var string
+     */
+    public $align_content = '';
+
+    /**
+     * The default block spacing.
+     *
+     * @var array
+     */
+    public $spacing = [
+        'padding' => null,
+        'margin' => null,
+    ];
+
+    /**
+     * The supported block features.
+     *
+     * @var array
+     */
+    public $supports = [
+        'align' => true,
+        'align_text' => false,
+        'align_content' => false,
+        'full_height' => false,
+        'anchor' => false,
+        'mode' => true,
+        'multiple' => true,
+        'jsx' => true,
+        'color' => [
+            'background' => false,
+            'text' => false,
+            'gradients' => false,
+        ],
+        'spacing' => [
+            'padding' => false,
+            'margin' => false,
+        ],
+    ];
+
+     /**
+     * Data to be passed to the block before rendering.
+     */
+    public function with(): array
+    {
+        return [
+            // Section Heading Fields
+            'section_eyebrow' => $this->getSectionEyebrow(),
+            'section_title' => $this->getSectionTitle(),
+            'section_description' => $this->getSectionDescription(),
+
+            // Content Fields (repeater)
+            'steps' => $this->getContentCards(), 
+
+            // Section Options
+            'section_size' => $this->getSectionSize(),
+            'theme' => $this->getTheme(),
+        ];
+    }
+
+     /**
+     * The block field group.
+     */
+    public function fields(): array
+    {
+        $stepCards = Builder::make('step_cards');
+
+        $stepCards
+            ->addMessage('Step Image Cards', 'A grid of cards with images, titles, and descriptions for showcasing features or key content.')
+            
+            ->addTab('Section Heading', [
+                'placement' => 'top',
+            ])
+            ->addPartial(SectionHeading::class)
+
+            ->addTab('Content', [
+                'placement' => 'top',
+            ])
+                ->addRepeater('steps', [
+                        'label' => 'Content Step Cards',
+                        'layout' => 'block',
+                        'button_label' => 'Add Step Card',
+                    ])
+                    ->addText('title', [
+                        'label' => 'Title',
+                        'instructions' => 'Card title',
+                    ])
+                    ->addTextarea('text', [
+                        'label' => 'Description',
+                        'instructions' => 'Brief description or excerpt',
+                        'rows' => 3,
+                    ])
+                    ->addImage('image', [
+                        'label' => 'Image',
+                        'instructions' => 'Card Image',
+                        'return_format' => 'array',
+                        'preview_size' => 'medium',
+                    ])
+                ->endRepeater()
+            ->addTab('Settings', [
+                'placement' => 'top',
+            ])
+            ->addPartial(SectionOptions::class);
+        return $stepCards->build();
+    }
+
+
+    /**
+     * Section heading getters
+     */
+    public function getSectionEyebrow()
+    {
+        return get_field('eyebrow');
+    }
+
+    public function getSectionTitle()
+    {
+        return get_field('heading');
+    }
+
+    public function getSectionDescription()
+    {
+        return get_field('subtitle');
+    }
+
+    /**
+     * Section content getters
+     */
+
+    public function getContentCards()
+    {
+        return get_field('steps') ?: [];
+    }
+
+    /**
+     * Section options getters
+     */
+    public function getSectionSize()
+    {
+        return get_field('section_size') ?: 'md';
+    }
+
+    public function getTheme()
+    {
+        return get_field('theme') ?: 'light';
+    }
+}
