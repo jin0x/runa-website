a1500a8372ebe0607f8f82dbb17b360b15f010b1
Liam Morris <LiamMorris96@gmail.com>
2025-10-01 13:28:07 +0100

    style: adjust testimonials

--- resources/views/blocks/testimonials-slider.blade.php
+++ resources/views/blocks/testimonials-slider.blade.php
@@ -22,6 +22,13 @@
       default => 'bg-primary-green-neon',
   };
 
+  // Convert theme string to ThemeVariant enum
+  $themeVariant = match ($theme) {
+      'dark' => ThemeVariant::DARK,
+      'green' => ThemeVariant::DARK,
+      default => ThemeVariant::LIGHT,
+  };
+
   // Set container size based on section size
   $containerSize = match ($section_size) {
       'xl' => ContainerSize::XLARGE,
@@ -34,34 +41,80 @@
       'light' => 'text-primary-dark',
       default => 'text-white',
   };
+  // Set container size based on layout
+  $containerSize = $display_layout === 'single' ? ContainerSize::MEDIUM : ContainerSize::XLARGE;
 @endphp
 
 @if($testimonials && count($testimonials) > 0)
   <x-section :size="$sectionSizeValue" classes="testimonials-slider-block {{ $bgColor }} {{ $block->classes ?? '' }}">
+
+    {{-- Section Heading --}}
+    @if($section_eyebrow || $section_title || $section_description)
+    <div class="justify-self-center mb-12 text-center">
+      <x-section-heading
+      :eyebrow="$section_eyebrow"
+      :heading="$section_title"
+      :subtitle="$section_description"
+      :variant="$themeVariant"
+      classes="mb-12"
+      />
+    </div>
+    @endif
+
     <x-container :size="$containerSize">
-
-      <x-slider
-        :navigation="$show_navigation"
-        :pagination="false"
-        :loop="true"
-        :autoplayDelay="$autoplay_delay * 1000"
-        :slidesPerView="1"
-        :mobileSlidesPerView="1"
-        :tabletSlidesPerView="2"
-        :desktopSlidesPerView="3"
-        :spaceBetween="24"
-        :navigationPosition="'bottom-right'"
-      >
-        @foreach($testimonials as $testimonial)
-          <div class="swiper-slide">
-            <x-testimonial-card
-              :post="$testimonial->ID"
-              :featured="false"
-            />
-          </div>
-        @endforeach
-      </x-slider>
-
+      @if($display_layout === 'single')
+        {{-- Single Testimonial Display with Slider --}}
+          <x-slider
+            :navigation="$show_navigation"
+            :pagination="false"
+            :loop="true"
+            :autoplayDelay="$autoplay_delay * 1000"
+            :slidesPerView="1"
+            :mobileSlidesPerView="1"
+            :tabletSlidesPerView="1"
+            :desktopSlidesPerView="1"
+            :spaceBetween="24"
+            :navigationPosition="'bottom-right'"
+          >
+            @foreach($testimonials as $testimonial)
+              <div class="swiper-slide">
+                <x-testimonial-card
+                  :post="$testimonial->ID"
+                  :featured="false"
+                  :cardColor="$card_background_color"
+                  :showLogo="$show_company_logos"
+                  :showRating="$show_ratings"
+                />
+              </div>
+            @endforeach
+          </x-slider>
+      @else
+        {{-- Slider Display --}}
+        <x-slider
+          :navigation="$show_navigation"
+          :pagination="false"
+          :loop="true"
+          :autoplayDelay="$autoplay_delay * 1000"
+          :slidesPerView="1"
+          :mobileSlidesPerView="1"
+          :tabletSlidesPerView="2"
+          :desktopSlidesPerView="2"
+          :spaceBetween="24"
+          :navigationPosition="'bottom-right'"
+        >
+          @foreach($testimonials as $testimonial)
+            <div class="swiper-slide">
+              <x-testimonial-card
+                :post="$testimonial->ID"
+                :featured="false"
+                :cardColor="$card_background_color"
+                :showLogo="$show_company_logos"
+                :showRating="$show_ratings"
+              />
+            </div>
+          @endforeach
+        </x-slider>
+      @endif
     </x-container>
   </x-section>
 @else
