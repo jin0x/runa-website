01508e861d09e22c849edbf37a2d40fb8dcdb9be
Liam Morris <LiamMorris96@gmail.com>
2025-10-02 17:59:10 +0100

    style: correct svg and spacing

--- resources/views/components/pricing-card.blade.php
+++ resources/views/components/pricing-card.blade.php
@@ -33,7 +33,7 @@
 
   // Popular card styling
   $cardClasses = $is_popular 
-    ? 'bg-gradient-5 border-2 border-primary-green-neon' 
+    ? 'bg-gradient-5 border-2 border-primary-green-soft' 
     : 'bg-neutral-500 border-2 border-transparent';
     
   $buttonVariant = $is_popular ? ButtonVariant::PRIMARY : ButtonVariant::SECONDARY;
@@ -113,7 +113,7 @@
 
   {{-- CTA Button --}}
   @if($cta && !empty($cta['url']) && !empty($cta['title']))
-    <div class="mb-6 pb-6">
+    <div class="pb-6 max-w-52">
       <x-button
         :variant="$buttonVariant"
         :href="$cta['url']"
@@ -127,7 +127,7 @@
 
   {{-- Features Section --}}
   @if($features_title || !empty($features))
-    <div class="pt-6">
+    <div class="pt-4">
       {{-- Features Title --}}
       @if($features_title)
         <x-text
@@ -145,9 +145,21 @@
           @foreach($features as $feature)
             <li class="flex items-center gap-2.5 min-h-12">
               {{-- Checkmark Icon --}}
-              <div class="flex-shrink-0 w-5 h-5 text-primary-green-neon">
-                <svg fill="currentColor" viewBox="0 0 20 20" class="w-full h-full">
-                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
+              <div class="">
+                <svg width="49" height="48" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <mask id="mask0_3073_19444" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="4" y="4" width="41" height="40">
+                    <rect x="4.33301" y="4" width="40" height="40" fill="#D9D9D9"/>
+                  </mask>
+                  <g mask="url(#mask0_3073_19444)">
+                    <path d="M18.7222 41.5L15.5555 36.1111L9.36112 34.8055L10.0278 28.6667L6 24L10.0278 19.3611L9.36112 13.2222L15.5555 11.9167L18.7222 6.5L24.3333 9.02779L29.9445 6.5L33.1389 11.9167L39.3055 13.2222L38.6389 19.3611L42.6667 24L38.6389 28.6667L39.3055 34.8055L33.1389 36.1111L29.9445 41.5L24.3333 38.9722L18.7222 41.5ZM19.9445 37.8889L24.3333 36.0278L28.8333 37.8889L31.4722 33.7778L36.25 32.5833L35.7778 27.7222L39.0556 24L35.7778 20.1945L36.25 15.3333L31.4722 14.2222L28.75 10.1111L24.3333 11.9722L19.8333 10.1111L17.1944 14.2222L12.4167 15.3333L12.8889 20.1945L9.61108 24L12.8889 27.7222L12.4167 32.6667L17.1944 33.7778L19.9445 37.8889ZM22.5555 29.6667L32 20.2778L29.9722 18.3333L22.5555 25.6945L18.7222 21.7778L16.6667 23.8055L22.5555 29.6667Z" fill="url(#paint0_linear_3073_19444)"/>
+                  </g>
+                  <defs>
+                    <linearGradient id="paint0_linear_3073_19444" x1="6" y1="41.5" x2="40.9622" y2="4.87297" gradientUnits="userSpaceOnUse">
+                      <stop stop-color="#00FFA3"/>
+                      <stop offset="0.48313" stop-color="#93FF82"/>
+                      <stop offset="0.943979" stop-color="#EEFC51"/>
+                    </linearGradient>
+                  </defs>
                 </svg>
               </div>
               
