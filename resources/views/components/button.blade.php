60746a071cadfded3798ab88d62dc8de65120e8a
Liam Morris <LiamMorris96@gmail.com>
2025-10-14 19:42:13 +0100

    fix: remove breaking nav button border (#69)

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -21,7 +21,7 @@
       ButtonVariant::PRIMARY => 'btn-primary text-primary-dark border-1 border-transparent hover:text-primary-light hover:border-primary-green-neon',
       ButtonVariant::SECONDARY => 'btn-secondary text-primary-light border-1 border-primary-green-neon transition-all duration-300 hover:text-primary-dark hover:border-transparent',
       ButtonVariant::LIGHT => 'btn-light text-primary-dark border-1 border-white transition-all duration-300 hover:bg-white hover:bg-opacity-100',
-      ButtonVariant::NAV => 'btn-nav border-1 border-transparent transition-all duration-300',
+      ButtonVariant::NAV => 'btn-nav transition-all duration-300',
   ];
 
   // Define background styles (using CSS custom properties for gradients)
