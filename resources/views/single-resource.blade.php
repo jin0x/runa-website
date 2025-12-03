e5bb8a8382fe5bc3d22901c1b56f6db74958d829
Liam Morris <LiamMorris96@gmail.com>
2025-12-03 16:43:54 +0000

    feat: single resource initial blade

---
+++ resources/views/single-resource.blade.php
@@ -0,0 +1,19 @@
+@php
+  use App\Enums\HeadingTag;
+  use App\Enums\HeadingSize;
+  use App\Enums\TextTag;
+  use App\Enums\TextSize;
+  use App\Enums\TextColor;
+@endphp
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) 
+    @php the_post() @endphp
+        {{-- Page Content --}}
+        <div class="prose max-w-none">
+          @php(the_content())
+        </div>
+  @endwhile
+@endsection
